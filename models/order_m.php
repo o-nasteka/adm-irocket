1e861873a74f58e644687129f7bcef0097742f26
Oleg Nasteka <oleg.nasteka@gmail.com>
2016-11-18 15:39:40 +0200

    UTM -> _POST

--- models/order_m.php
+++ models/order_m.php
@@ -231,9 +231,8 @@ class Order_m extends Model {
                        comment2 = '{$_POST['comment2']}',
                        phone = '{$_POST['phone']}',
                        user_name = '{$_SESSION['login']}',
-                       utm = '{$_SESSION['utm']}',
+                       utm = '{$_POST['utm']}',
                        status = '{$_POST['status']}'
-
 
                    where `id` = {$id}
             ";
@@ -267,7 +266,7 @@ class Order_m extends Model {
                        comment1 = '{$_POST['comment1']}',
                        comment2 = '{$_POST['comment2']}',
                        user_name = '{$_SESSION['login']}',
-                       utm = '{$_SESSION['utm']}',
+                       utm = '{$_POST['utm']}',
                        date = '{$date}'
 
             ";
